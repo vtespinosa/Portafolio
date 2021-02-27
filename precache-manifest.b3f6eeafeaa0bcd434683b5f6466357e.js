c3ca0b51c3387fada70d4d921b52f02d7610e97f
Vicente Espinosa <vtespinosa@uc.cl>
2021-02-27 11:52:33 -0300

    Updates

---
+++ precache-manifest.b3f6eeafeaa0bcd434683b5f6466357e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151f23b46fc250d96e89f302167f8e0c",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "d13696048b4e62136a0a",
+    "url": "/Portafolio/static/css/main.914889bc.chunk.css"
+  },
+  {
+    "revision": "bd705e7bd3bc7a7d164e",
+    "url": "/Portafolio/static/js/2.c2df45c3.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Portafolio/static/js/2.c2df45c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13696048b4e62136a0a",
+    "url": "/Portafolio/static/js/main.c4fc3040.chunk.js"
+  },
+  {
+    "revision": "9b334899946831439716",
+    "url": "/Portafolio/static/js/runtime-main.08e0be74.js"
+  },
+  {
+    "revision": "fc3e1d3c67143d54449ea7a5d642b6f7",
+    "url": "/Portafolio/static/media/BackEnd.fc3e1d3c.svg"
+  },
+  {
+    "revision": "baa06e198bf2231e66ad7e84d851eadc",
+    "url": "/Portafolio/static/media/DataScience.baa06e19.svg"
+  },
+  {
+    "revision": "7c9a93b9054f13eae11fff6bb2f067dd",
+    "url": "/Portafolio/static/media/FrontEnd.7c9a93b9.svg"
+  },
+  {
+    "revision": "188bad7e9d19eb849362f7ce1a10f030",
+    "url": "/Portafolio/static/media/larbos.188bad7e.png"
+  },
+  {
+    "revision": "3ab2ebb1996c049799d73f342ab23dcd",
+    "url": "/Portafolio/static/media/patagonia.3ab2ebb1.jpg"
+  }
+]);
